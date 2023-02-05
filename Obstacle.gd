783c886ae3ea9ce3428bd6777e9c41059776f455
Claudio Caruzo <cgcaruzo@gmail.com>
2023-02-05 12:13:06 -0300

    moficiaciones claudio domingo 12:13

--- Obstacle.gd
+++ Obstacle.gd
@@ -2,27 +2,39 @@ extends Area2D
 
 var speed = 400
 var collided = false
-
+onready var sprites_children = $Sprites.get_children()
+onready var sprites_lenght = $Sprites.get_child_count()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	generate_sprite()
 	pass # Replace with function body.
 
 
 func _process(delta):
+
 	# Mover el obstáculo hacia la izquierda
 	position.x -= speed * delta
 
 	# Reiniciar la posición si se sale de la pantalla
 	if position.x < -40:
 		var randomPosition = randi() % 2
+		
 #		print(randomPosition)
 		if randomPosition == 0:
 			position.y = $Spawners/BottomSpawner.global_position.y
+			$Sprite.flip_v = false
+#			$Sprite.material.set_shader_param("offset", Vector2(-5,8))
 		else:
 			position.y = $Spawners/TopSpawner.global_position.y	
+			$Sprite.flip_v = true
+#			$Sprite.material.set_shader_param("offset", Vector2(5,-8))
+		generate_sprite()
 		print(position.y)
 		position.x = 1100
 		collided = false
  
-
+func generate_sprite():
+	var randomSpriteIndex = randi() % sprites_lenght
+	$Sprite.texture = sprites_children[randomSpriteIndex].texture
+	$Sprite.region_rect = sprites_children[randomSpriteIndex].region_rect
